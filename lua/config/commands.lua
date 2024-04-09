67edd94525881e1e11e58c87ab147fe3dcd6d7d9
Ashish Bhattarai <ashishbhattarai@protonmail.com>
2024-04-09 17:34:22 +0700

    Add command for running specific jest test case

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,13 +1,19 @@
-function RunPrettier()
+
+-- JS tool commands
+
+function runPrettier()
     local filename = vim.fn.expand('%:p')
     local command = 'npx prettier "' .. filename .. '" --write'
     vim.fn.system(command)
 end
 
 local jest_terminal_buffer = -1
-function RunJest()
+local function runJest(spec_name)
   local fname = vim.fn.expand('%:p')
-  local command = 'npx jest ' .. fname
+  local test_name = spec_name and ' -t \'' .. spec_name .. '\'' or ''
+  local command = 'npx jest ' .. fname .. test_name
+
+  vim.api.nvim_err_writeln(command)
 
   -- If terminal buffer exists, delete it
   if jest_terminal_buffer ~= -1 and vim.fn.bufexists(jest_terminal_buffer) == 1 then
@@ -19,5 +25,23 @@ function RunJest()
   vim.cmd('wincmd p')
 end
 
-vim.cmd('command! PrettierCurrentFile lua RunPrettier()')
-vim.cmd('command! RunJest lua RunJest()')
+function getCurrentLineText()
+  local current_line = vim.api.nvim_get_current_line()
+  local pattern = '%s*"([^"]+)"'
+  local captured_text = current_line:match(pattern)
+  return captured_text
+end
+
+function runJestSpec() 
+  local spec_name = getCurrentLineText();  
+  return runJest(spec_name);
+end
+function runJestFile()
+  return runJest(nil);
+end
+
+vim.api.nvim_create_user_command('RunPrettier', runPrettier, {})
+vim.api.nvim_create_user_command('RunJest', runJestFile, {})
+vim.api.nvim_create_user_command('RunJestSpec', runJestSpec, {})
+
+-- 
