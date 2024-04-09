22477714d6e8993a57ba1195d5e495f2b36baa6d
Ashish Bhattarai <ashishbhattarai@protonmail.com>
2024-04-09 17:39:08 +0700

    Backup local js config

---
+++ .js_nvim.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('n', '<A-F>', ':RunPrettier<CR>', { silent = true })
+vim.keymap.set('n', '<leader>tf', ':RunJest<CR>', { silent = true })
+vim.keymap.set('n', '<leader>tt', ':RunJestSpec<CR>', { silent = true })
+
