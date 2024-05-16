0413fc00fb5c3453eb35591daaa7369e988acedb
Ashish Bhattarai <ashishbhattarai@protonmail.com>
2024-05-16 13:49:50 +0700

    telecope, set filename to be displayed upfront

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -43,6 +43,11 @@ return {
       	  height = 0.80,
       	  preview_cutoff = 120,
       	},
+        path_display = {
+          filename_first = {
+            reverse_directories = false
+          }
+        },
         mappings = {
           i = {
       	    ["<C-s>"] = "select_horizontal"
