f89b149d6dbc4eef685b6097a60eabffc596f127
Ashish Bhattarai <ashishbhattarai@protonmail.com>
2024-05-28 14:06:25 +0700

    Add binding for fuzzy find current buffer

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -61,8 +61,8 @@ return {
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<A-p>', builtin.find_files, {})
       vim.keymap.set('n', '<A-P>', builtin.live_grep, {})
-      vim.keymap.set('n', '<A-b>', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+      vim.keymap.set('n', '<leader>f', builtin.current_buffer_fuzzy_find, {})
+      vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
     end
   },
   {
